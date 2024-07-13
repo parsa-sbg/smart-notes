55119531b5680d3c14ab836d0fca55a04fb42ea7
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-07-13 20:11:34 +0330

    develop pricing switch logic

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const mobileMenuBtn = document.querySelector('.mobile-menu-btn')
 const menu = document.querySelector('.menu')
+const yearlyPricingSwitch = document.querySelector('.pricing__swich')
+const planPricePertimeElem = document.querySelectorAll('.plan__price-pertime')
+const planPriceNumberElem = document.querySelectorAll('.plan__price-number')
 
 
+//////////////////////// funcs ////////////////////////////////////////
 const handleOpenMobileMenu = () => {
     menu.classList.toggle('menu--open')
     const mobileMenuBtnLine = mobileMenuBtn.querySelector('div')
     mobileMenuBtnLine.classList.toggle('mobile-menu-btn__line--open')
 }
 
+const changePricePerTime = (isYear) => {
+  if(isYear) {
+    planPricePertimeElem.forEach(elem => {
+      elem.innerHTML = '/ year'
+    })
+    planPriceNumberElem[0].innerHTML = '$89.9'
+    planPriceNumberElem[1].innerHTML = '$239.9'
+  }else {
+    planPricePertimeElem.forEach(elem => {
+      elem.innerHTML = '/ month'
+    })
+    planPriceNumberElem[0].innerHTML = '$9.99'
+    planPriceNumberElem[1].innerHTML = '$19.99'
+  }
+}
 
 
-mobileMenuBtn.addEventListener('click', () => {
-    console.log('click');
-    handleOpenMobileMenu()
-})
-
-
+// handle swiper
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     slidesPerView: 5,
@@ -53,3 +67,20 @@ const swiper = new Swiper('.swiper', {
         }
       }
 });
+
+
+/////////////////////// events /////////////////////////////////////////
+
+mobileMenuBtn.addEventListener('click', () => {
+  console.log('click');
+  handleOpenMobileMenu()
+})
+
+yearlyPricingSwitch.addEventListener('change', event => {
+
+  if (event.target.checked) {
+    changePricePerTime(true)
+  }else{
+    changePricePerTime(false)
+  }
+})
